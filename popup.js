f7655acb925738b00277a4858023a7384bc26fa0
Aagam Jain <aagamjain@Aagams-MacBook-Air.local>
2022-07-11 18:33:15 +0530

    v1.0.1.4.5

--- popup.js
+++ popup.js
@@ -68,7 +68,7 @@ function check(val){
      if(colorArr[i]==val)
      return false;
    }
-   console.log("true");
+  //  console.log("true");
    return true;
 }
 
@@ -236,8 +236,69 @@ function getColor(){
 
 
 
+// convertors ==========>
+function HexToHSL(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
 
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; 
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        
+        h /= 6;
+    }
+
+    s = s*100;
+    s = Math.round(s);
+    l = l*100;
+    l = Math.round(l);
+    h = Math.round(360*h);
+
+    return {h, s, l};
+}
+
+function hslToHex(h, s, l) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
 
 
 function hexToRgb(hex) {
@@ -252,6 +313,7 @@ function hexToRgb(hex) {
 function rgbToHex(r, g, b) {
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
+
 
 // Color  converter --------->
 
@@ -274,7 +336,7 @@ HEXvalue.addEventListener('input',()=>{
     newTemp = "#"+newTemp;
     hexRESULT.innerHTML = newTemp;
     var t = hexToRgb(newTemp);
-    console.log(t);
+    // console.log(t);
     if(t!=null)
     {RGBresult.innerHTML = `rgb(${t.r}, ${t.g}, ${t.b})`;}
 })
@@ -309,3 +371,82 @@ hexRESULT.addEventListener('click',()=>{
   navigator.clipboard.writeText(hexRESULT.innerText);
   show_toast("Color Picked");
 })
+
+
+// Color Grader  ------------------>
+
+const cursorRounded = document.querySelector('.rounded');
+const nowCol = document.querySelector('.nowCol');
+const givCol = document.querySelector('.colGiven');
+const colorGrader1 = document.querySelector('#HEXvalueColorGrader');
+const colorGrader2 = document.querySelector('#colorValueColorGrader');
+const con = document.querySelector('.colorGrader');
+
+colorGrader1.addEventListener('input',()=>{
+    var temp = colorGrader1.value.toLowerCase();
+    var newTemp = "";
+    for(let i=0;i<temp.length;i++)
+    {
+      if((temp[i]<='9'&&temp[i]>='0') || (temp[i]<='f'&&temp[i]>='a'))
+      newTemp+= temp[i]
+    }
+    colorGrader1.value = newTemp;
+    newTemp = "#"+newTemp;
+    var t = hexToRgb(newTemp);
+    // console.log(t);
+    if(t!=null)
+    {colorGrader2.value = newTemp; colorGrader2.dispatchEvent(new Event('input'));}
+})
+colorGrader2.addEventListener('input',()=>{
+    // console.log("change Occur");
+    let inputColor = colorGrader2.value;
+    let ourColor = HexToHSL(inputColor);
+    var temp = {};
+    temp[ourColor.l] = hslToHex(ourColor.h,ourColor.s,ourColor.l);
+    for(var i = 100 ;i >= 0 ; i -= 4)
+    {
+        temp[i] = hslToHex(ourColor.h,ourColor.s,i);
+    }
+    con.innerHTML = ``;
+    for (const [key, value] of Object.entries(temp)) {
+        var fontColor;
+        if(key<=50)
+        fontColor = "white";
+        else
+        fontColor = "black";
+        var colorHigh = ((key == ourColor.l)?"ColorHigh":"");
+        con.innerHTML = `
+            <div class="colorBox  ${colorHigh}" id="cID${key}" style="color: ${fontColor}; background-color: ${value};">
+                 <span class="keySpan" id="keySpanid${key}"></span>
+                 ${value}
+            </div>
+            
+        ` + con.innerHTML;
+    }
+    for (const [key, value] of Object.entries(temp)) {
+        document.querySelector(`#cID${key}`).addEventListener('mousemove',(e)=>{
+            document.querySelector(`#keySpanid${key}`).innerHTML = `${key}%`;
+            cursorRounded.style.display = "block";
+            nowCol.style.backgroundColor = `${value}`;
+            givCol.style.backgroundColor = `${inputColor}`;
+            const mouseY = e.clientY;
+            const mouseX = e.clientX;
+            cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+      })
+        document.querySelector(`#cID${key}`).addEventListener('mouseout',(e)=>{
+          document.querySelector(`#keySpanid${key}`).innerHTML = ``;
+          cursorRounded.style.display = "none";
+        })
+        document.querySelector(`#cID${key}`).addEventListener('click',(e)=>{
+          navigator.clipboard.writeText(value);
+          show_toast("Color Picked");
+        })
+    }
+})
+
+
+
+
+
+
+
